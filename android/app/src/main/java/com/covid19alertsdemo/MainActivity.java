fb138e10be6e5ebdad026a72447af6171f2d68de
MaradiSu <p.suresh@campusuvce.in>
2021-06-07 04:00:14 +0530

    added splash activity

--- android/app/src/main/java/com/covid19alertsdemo/MainActivity.java
+++ android/app/src/main/java/com/covid19alertsdemo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.covid19alertsdemo;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
+
 
 public class MainActivity extends ReactActivity {
 
@@ -8,6 +11,11 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+   @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this, R.style.SplashStatusBarTheme);
+    super.onCreate(savedInstanceState);
+  }
   @Override
   protected String getMainComponentName() {
     return "covid19alertsdemo";
